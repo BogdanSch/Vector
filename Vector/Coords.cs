1a4960a2b0b99d5d059e43bdd10f09fce7f53346
Kit <Kit@KIT11-15>
2021-11-13 17:21:02 +0200

    Добавьте файлы проекта.

---
+++ Vector/Coords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorLib
+{
+    public struct  Coords
+    {
+        public Coords(double x = 0, double y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
